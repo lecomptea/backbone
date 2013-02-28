84c10c01c4e5d532f7d85457fd2f481e1fcf8a5d
Antoine Lecompte <lecomptea28@gmail.com>
2013-02-28 08:03:40 -0500

    added readCookie function.

--- backbone.js
+++ backbone.js
@@ -6,7 +6,19 @@
 //     http://backbonejs.org
 
 (function(){
-
+  
+  //readCookie function to get document cookies.
+  var readCookie = function(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+  
   // Initial Setup
   // -------------
 
